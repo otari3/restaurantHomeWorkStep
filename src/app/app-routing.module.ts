3f7b409359e2efc87138bad363222f4b0c9e17fb
otari nozadze <otikonozadze@gmail.com>
2024-05-10 06:46:01 +0400

    set up very basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategorisOptionsComponent } from './categories/categoriesOptions/categoris-options/categoris-options.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'categoris', pathMatch: 'full' },
+  { path: 'categoris', component: CategorisOptionsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
