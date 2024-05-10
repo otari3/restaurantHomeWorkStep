10c801605dcfec335664a882fc13a158bb9820ba
otari nozadze <otikonozadze@gmail.com>
2024-05-10 07:41:47 +0400

    finisshed categoryFilter

--- src/app/categories/foodDisplay/food-display/food-display.component.ts
+++ src/app/categories/foodDisplay/food-display/food-display.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { HandelingStateService } from '../../../shared/handelingState/handeling-state.service';
+import { ApiCallsService } from '../../../shared/apiService/api-calls.service';
+import { Product } from '../../../shared/apiService/types/types';
 
 @Component({
   selector: 'app-food-display',
@@ -10,11 +12,22 @@ import { HandelingStateService } from '../../../shared/handelingState/handeling-
 export class FoodDisplayComponent implements OnInit {
   constructor(
     private activRoute: ActivatedRoute,
-    private state: HandelingStateService
+    private state: HandelingStateService,
+    private api: ApiCallsService
   ) {}
+  products!: Product[];
   updatingCurrentFilter() {
     this.activRoute.params.subscribe((data: Params) => {
       this.state.gettingCurentFilter.next(data['id']);
+      if (data['id'] === 'all') {
+        this.api.gettingAllProduts().subscribe((data: Product[]) => {
+          this.products = data;
+        });
+      } else {
+        this.api.gettingCategoryProducts(+data['id']).subscribe((data) => {
+          this.products = data.products;
+        });
+      }
     });
   }
   ngOnInit(): void {
--- src/app/shared/apiService/api-calls.service.ts
+++ src/app/shared/apiService/api-calls.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Catergoris } from './types/types';
+import { Catergoris, Product, Products } from './types/types';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +9,16 @@ export class ApiCallsService {
   gettingAllCategoris() {
     return this.http.get<Catergoris[]>(
       'https://restaurant.stepprojects.ge/api/Categories/GetAll'
+    );
+  }
+  gettingAllProduts() {
+    return this.http.get<Product[]>(
+      'https://restaurant.stepprojects.ge/api/Products/GetAll'
+    );
+  }
+  gettingCategoryProducts(id: number) {
+    return this.http.get<Products>(
+      `https://restaurant.stepprojects.ge/api/Categories/GetCategory/${id}`
     );
   }
 
